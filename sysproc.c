954c48630e8e469d9da977c9792c4d3813cbe379
HirokiMatsume <matsume-h@goods-inc.jp>
2018-10-25 13:44:37 +0900

    malloc

--- sysproc.c
+++ sysproc.c
@@ -111,8 +111,8 @@ sys_sleep_sec(void)
   int n;
   struct rtcdate *t1, *t2;
 
-  if (argptr(0, (char **)&t1, sizeof(t1)) < 0 || argptr(0, (char **)&t2, sizeof(t2)) < 0)
-    return -1;
+  t1 = malloc(sizeof(*t1));
+  t2 = malloc(sizeof(*t2));
 
   cmostime(t1);
   cmostime(t2);
