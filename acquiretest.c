39176b91560311b232becffd83fe903856a8bbcc
HirokiMatsume <matsume-h@goods-inc.jp>
2018-11-08 12:51:54 +0900

    add acquiretest

---
+++ acquiretest.c
@@ -0,0 +1,30 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+#define N 1000
+
+int sleept(int ti, int pid) {
+  int n = 10;
+  while (n > 0)
+  {
+    printf(1, "pid%d: %d: %d\n", pid, n--, uptime());
+    sleep(ti);
+  }
+  exit();
+}
+
+int main(void) {
+  int n = 100, pid;
+  int i, j;
+
+  pid = fork();
+  if (pid == 0) {
+    sleept(n, pid);
+    wait();
+  } else {
+    sleept(n, pid);
+    exit();
+  }
+  exit();
+}
